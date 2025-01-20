f9056e532ae0af140a41d603a6c7adbce601bbaf
Fernando Leon Franco <fernanlee2131@gmail.com>
2025-01-20 06:43:24 -0600

    Nice

--- optimizacion/clases/warm_up.py
+++ optimizacion/clases/warm_up.py
@@ -18,11 +18,18 @@ BM = [[5,6],[7,8]]
 
 
 def matriz(a,b):
-    vector = [[0,0],[0,0]]
-    for i in range(len(a)):
-        for k in range(len(b)):
-            vector[i][k]= (a[i][k] * b[k][i]) + (a[i][k] * b[k+1][i])
+
+    filas_a, columnas_a = len(a), len(a[0])
+    filas_b, columnas_b = len(b), len(b[0])
+    
+    vector = [[0 for _ in range(columnas_b)] for _ in range(filas_a)]
+    
+    for i in range(filas_a):
+        for j in range(columnas_b):
+            for k in range(columnas_a):
+                vector[i][j] +=  a[i][k] * b[k][j]
     print(vector)
+
 
 punto(a,b)
 matriz(AM,BM)
