ad327ba2ee27b88a32c1921798c4d7ec7af010af
Fernando Leon Franco <fernanlee2131@gmail.com>
2025-02-01 22:23:52 -0600

    Version dos dora mejora y pinta pero no es lo optimo

--- graphic_visualization/tareas/tarea_3/generador_linea.py
+++ graphic_visualization/tareas/tarea_3/generador_linea.py
@@ -1,40 +1,43 @@
+from rich.console import Console
+from rich.table import Table
 import numpy as np
 
 
-
+console = Console()
 
 tamano = 20
-
 cord_uno = [2, 4]
 cord_dos = [12, 18]
 
 
 
-matriz = np.array([["░" for i in range(tamano)] for i in range(tamano)], dtype=str)
+def crear_matriz(tamano):
+    return np.array([["░" for _ in range(tamano)] for _ in range(tamano)], dtype=str)
 
-
-for fila in matriz:
-    print(" ".join(fila))
-    
-print("\n")
-
-
-columnas = len(matriz[0])
-filas = len(matriz)
-
-
-def pintar_pixel(x,y):
-    return [x,y] == cord_uno
+def pintar_pixel(x, y,cord_uno,cord_dos):
+    x_1,y_1 = cord_uno
+    x_2,y_2 = cord_dos
+    A = y_2 - y_1
+    B = x_1 - x_2
+    C = (x_2 * y_1) - (x_1*y_2)
+    resultado = (A*x) + (B*y) + C 
+    return 1 > resultado >= 0
 
 
 
+def modificar_matriz(matriz):
+    for i in range(tamano):
+        for j in range(tamano):
+            if pintar_pixel(i, j,cord_uno,cord_dos):
+                matriz[i][j] = "█"
+    return matriz
 
+def imprimir_matriz(matriz):
+    table = Table(show_header=False, show_lines=False, expand=True)
+    for fila in matriz:
+        table.add_row(" ".join(f"[bold green]{c}[/]" if c == "█" else f"[dim]{c}[/]" for c in fila))
+    console.print(table)
 
-for i,_ in enumerate(range(filas)):
-    for j,_ in enumerate(range(filas)):
-        if pintar_pixel(i,j):
-            matriz[i][j] = "█"
-
-
-for fila in matriz:
-    print(" ".join(fila))
+matriz = crear_matriz(tamano)
+matriz = modificar_matriz(matriz)
+imprimir_matriz(matriz)
