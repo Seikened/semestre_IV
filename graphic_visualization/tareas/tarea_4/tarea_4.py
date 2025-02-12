132c187b54c51040e4949a94b342e4dadba5a470
Fernando Leon Franco <fernanlee2131@gmail.com>
2025-02-12 16:56:34 -0600

    Tarea  de dora terminada

--- graphic_visualization/tareas/tarea_4/tarea_4.py
+++ graphic_visualization/tareas/tarea_4/tarea_4.py
@@ -1,8 +1,22 @@
+import pygame
 from rich.console import Console
 from rich.table import Table
 import numpy as np
+import enum
+import sys
 
 console = Console()
+
+
+class Confi(enum.Enum):
+    tamano = 50
+    tam_pix = 10
+    cord_uno = [2, 4]    
+    cord_dos = [48, 20]
+    blanco = (255,255,255)
+    gris = (200,200,200)
+    color_pixel = (155, 89, 182)
+    
 
 def crear_matriz(tamano):
     return np.array([["░" for _ in range(tamano)] for _ in range(tamano)], dtype=str)
@@ -37,10 +51,108 @@ def bresenham(matriz, p1, p2):
     return matriz
 
 
-tamano = 50
-cord_uno = [2, 4]    
-cord_dos = [48, 20]  
 
-matriz = crear_matriz(tamano)
-matriz = bresenham(matriz, cord_uno, cord_dos)
-imprimir_matriz(matriz)
+def circulo(cx,cy, r):
+    circulo = []
+    x = 0
+    y = -r
+    p = -r
+    while x < -y:
+        if p > 0:
+            y += 1
+            p += 2*(x+y) + 1
+        else:
+            p += 2*x + 1    
+        
+        circulo.append([cx + x, cy + y])
+        circulo.append([cx - x, cy + y])
+        circulo.append([cx + x, cy - y])
+        circulo.append([cx - x, cy - y])
+        circulo.append([cx + y, cy + x])
+        circulo.append([cx + y, cy - x])
+        circulo.append([cx - y, cy + x])
+        circulo.append([cx - y, cy - x])
+        
+        x += 1
+        
+    return circulo
+        
+    
+
+
+
+pygame.init()
+
+v_an = 800
+v_al = 600
+
+ventana = pygame.display.set_mode((v_an,v_al))
+pygame.display.set_caption("Hola fer")
+
+punto_inicial = None
+
+def dibujar_rejilla():
+    for x in range(0,v_an,Confi.tam_pix.value):
+        pygame.draw.line(ventana,Confi.gris.value,(x,0),(x,v_al))
+    for y in range(0,v_al,Confi.tam_pix.value):
+        pygame.draw.line(ventana,Confi.gris.value,(0,y),(v_an,y))
+
+def dibujar_pixel(x,y,color= Confi.color_pixel.value):
+    pygame.draw.rect(ventana,color,(x*Confi.tam_pix.value,y*Confi.tam_pix.value,Confi.tam_pix.value,Confi.tam_pix.value))
+
+
+# while True:
+#     for evento in pygame.event.get():
+#         if evento.type == pygame.QUIT:
+#             pygame.quit()
+#             sys.exit()
+#         elif evento.type == pygame.MOUSEBUTTONDOWN:
+#             x, y = pygame.mouse.get_pos()
+#             punto_inicial = (x // Confi.tam_pix.value, y // Confi.tam_pix.value)
+
+#     ventana.fill(Confi.blanco.value)
+#     dibujar_rejilla()
+
+#     if punto_inicial is not None:
+#         # Obtenemos la posición actual del mouse
+#         x, y = pygame.mouse.get_pos()
+#         mouse = (x // Confi.tam_pix.value, y // Confi.tam_pix.value)
+
+#         # Creamos la matriz y aplicamos el algoritmo de Bresenham
+#         matriz = crear_matriz(Confi.tamano.value)
+#         matriz = bresenham(matriz, punto_inicial, mouse)
+
+#         # Dibujamos solo los píxeles marcados con "█"
+#         for i in range(Confi.tamano.value):
+#             for j in range(Confi.tamano.value):
+#                 if matriz[i][j] == "█":
+#                     dibujar_pixel(j, i)
+
+#     pygame.display.flip()
+    
+circulos = []
+
+while True:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif evento.type == pygame.MOUSEBUTTONDOWN:
+            x, y = pygame.mouse.get_pos()
+            grid_x = x // Confi.tam_pix.value
+            grid_y = y // Confi.tam_pix.value
+            # Almacena los puntos del círculo al hacer clic
+            puntos_circulo = circulo(grid_x, grid_y, r=6)
+            circulos.append(puntos_circulo)
+
+    ventana.fill(Confi.blanco.value)
+    dibujar_rejilla()
+
+    # Dibuja todos los círculos almacenados
+    for puntos_circulo in circulos:
+        for (j, i) in puntos_circulo:
+            dibujar_pixel(j, i)
+
+    pygame.display.flip()
+
+
