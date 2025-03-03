c6fa38798ce42c59e8ecc11fe69259cdb1c96619
Fernando Leon Franco <fernanlee2131@gmail.com>
2025-03-02 22:05:14 -0600

    v2 tarea dora no jala   :(

--- graphic_visualization/tareas/tarea_6/imagen_panoramica.py
+++ graphic_visualization/tareas/tarea_6/imagen_panoramica.py
@@ -24,14 +24,31 @@ kp2, des2 = orb.detectAndCompute(img2_gray, None)
 print(f"Numero de puntos clave en la imagen 1: {len(kp1)} | Descriptores: {des1.shape}")
 print(f"Numero de puntos clave en la imagen 2: {len(kp2)} | Descriptores: {des2.shape}")
 
+
+
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
+matches = bf.knnMatch(des1, des2, k=2)
+good_matches = []
+for m,n  in matches:
+    if m.distance < 0.75 * n.distance:
+        good_matches.append(m)
+print(f"Numero de puntos clave emparejados: {len(good_matches)}")
+
+pts1 = np.float32([kp1[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+pts2 = np.float32([kp2[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+
+H, mask = cv2.findHomography(pts1, pts2, cv2.RANSAC, 5.0)
+print(f"Matriz de homografia:\n{H}")
+
+alto, ancho = img2.shape[:2]
+
+resultado = cv2.warpPerspective(img1, H, (ancho, alto))
+resultado[0:alto, 0:ancho] = img2
+
+
 # Vamos a dibujar los puntos clave en las imagenes
-img1_kp = cv2.drawKeypoints(img1_gray, kp1, None, color=(0, 255, 0), flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-img2_kp = cv2.drawKeypoints(img2_gray, kp2, None, color=(0, 255, 0), flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-plt.figure(figsize=(10, 5))
-plt.subplot(1, 2, 1)
-plt.imshow(cv2.cvtColor(img1_kp, cv2.COLOR_BGR2RGB))
-plt.title("Imagen 1")
-plt.subplot(1, 2, 2)
-plt.imshow(cv2.cvtColor(img2_kp, cv2.COLOR_BGR2RGB))
-plt.title("Imagen 2")
+plt.figure(figsize=(20, 10))
+plt.imshow(cv2.cvtColor(resultado, cv2.COLOR_BGR2RGB))
+plt.title("Imagen panoramica")
+plt.axis("off")
 plt.show()
