c99e4201da3f3f820004e550e4fadeac50717777
Fernando Leon Franco <fernanlee2131@gmail.com>
2025-04-27 21:14:02 -0600

    Implement code changes to enhance functionality and improve performance

--- optimization/proyectos/proyecto_final.py
+++ optimization/proyectos/proyecto_final.py
@@ -4,13 +4,16 @@ try:
     from mathkat import Gradiente
     from colorstreak import log
     import numpy as np
+    from PIL import Image, ImageFilter
+
 except ImportError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "mathkat"])
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "colorstreak"])
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "numpy"])
+
+    librerias_faltantes = ['mathkat', 'colorstreak', 'numpy', 'Pillow']
+    subprocess.check_call([sys.executable, "-m", "pip", "install", *librerias_faltantes]) 
     from mathkat import Gradiente 
     from colorstreak import log
     import numpy as np
+    from PIL import Image, ImageFilter
 
 def inicializador():
     log.info('LAS LIBRERIAS "mathkat", "colorstreak"  son mias puedes consultarlo con el comando "pip show mathkat" y "pip show colorstreak"')
@@ -29,22 +32,71 @@ def inicializador():
 
 
 
+from dataclasses import dataclass , field
+
+@dataclass
+class Imagen:
+    ruta: str
+    ancho: int = field(init=False)
+    alto: int = field(init=False)
+    imagen: any = field(init=False)
+
+    def __post_init__(self):
+        self.cargar_imagen()
+        log.info(f'Imagen cargada: {self.ruta}')
+        log.info(f'Tamaño de la imagen: {self.ancho}x{self.alto}')
+    
+    
+    def cargar_imagen(self):
+
+        self.imagen = Image.open(self.ruta)
+        self.ancho, self.alto = self.imagen.size
+
+    def mostrar(self):
+        self.imagen.show()
+
+    def cambiar_tam(self, nuevo_ancho, nuevo_alto):
+        self.imagen = self.imagen.resize((nuevo_ancho, nuevo_alto))
+        self.ancho, self.alto = self.imagen.size
+
+    def guardar_img(self, nueva_ruta):
+        self.imagen.save(nueva_ruta)
+
+    def aplicar_ruido_al_pixel(self, ruido):
+        
+        self.imagen = self.imagen.filter(ImageFilter.GaussianBlur(ruido))
+
+
+
+
 
 # ================================================ PROYECTO FINAL ===========================================================
-inicializador()
+#inicializador()
 
 
-# Ejemplo de uso
-f = lambda x, y: (x-1)**2 + (y-2)**2
-grad_f = lambda x, y: np.array([2*(x-1), 2*(y-2)])
-x_0 = np.array([0.0, 0.0])
-v_0 = np.array([0.0, 0.0])
-alpha = 0.1
-iteraciones = 10
-epsilon = 1e-6
-eta = 0.9
+# # Ejemplo de uso
+# f = lambda x, y: (x-1)**2 + (y-2)**2
+# grad_f = lambda x, y: np.array([2*(x-1), 2*(y-2)])
+# x_0 = np.array([0.0, 0.0])
+# v_0 = np.array([0.0, 0.0])
+# alpha = 0.1
+# iteraciones = 10
+# epsilon = 1e-6
+# eta = 0.9
 
-funcion = Gradiente(f, grad_f, x_0, v_0, alpha, iteraciones, epsilon, eta)
-funcion.simple()
-funcion.momentum()
-funcion.nesterov()
+# funcion = Gradiente(f, grad_f, x_0, v_0, alpha, iteraciones, epsilon, eta)
+# funcion.simple()
+# funcion.momentum()
+# funcion.nesterov()
+
+
+funcion_objetivo = lambda u,f,λ,u_grad: ( 0.5 * (abs(u - f)**2) ) + ((λ/2) * (abs(u_grad)**2) )
+
+
+
+# # Ejemplo de uso
+ruta_base = '/Users/ferleon/Documents/GitHub/semestre_IV/optimization/proyectos/'
+ruta_img = ruta_base + 'fer.jpeg'
+imagen = Imagen(ruta_img)
+
+imagen.aplicar_ruido_al_pixel(3)
